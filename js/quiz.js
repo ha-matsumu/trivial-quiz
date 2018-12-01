3f1932003296b40ae64e6729654f8db60f83d67f
ha-matsumu <ha.matsumu@gmail.com>
2018-12-01 20:32:18 +0900

    updated fetch-quiz feature

--- js/quiz.js
+++ js/quiz.js
@@ -3,13 +3,10 @@ function fetchQuiz() {
     return fetch('https://opentdb.com/api.php?amount=10&category=18&type=multiple')
     .then(response => {
         return response.json();
-    })
-    .then(myJson => {
-        return JSON.stringify(myJson);
     });
 }
 
 // 返り値(promiseオブジェクト)から値を取得する
-fetchQuiz().then(results => {
-    console.log(results);
+fetchQuiz().then(response => {
+    console.log(response);
 });
